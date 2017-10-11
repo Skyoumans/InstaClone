60d7766e0f3c3549edb4d3edf33e5fc41c2491d3
Skyoumans <skyyoumans@gmail.com>
2017-10-11 11:52:05 -0400

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import { FaInstagram, FaUser, FaHeartO, FaCompass, FaSearch } from 'react-icons/lib/fa';
 import styled from 'styled-components';
 
+
+const mainImage = styled.p`
+  display: block;
+  -webkit-margin-before: 1em;
+    -webkit-margin-after: 1em;
+    -webkit-margin-start: 0px;
+    -webkit-margin-end: 0px;
+    text-align: center;
+    font-size: 100px;
+`;
+const mainSearch = styled
+    
+
 class Navbar extends Component{
   render(){
     return (
       <div>
+      <mainImage>        
         <h3><FaInstagram /> | Instaclone</h3>
+        </mainImage>      
         <FaSearch/><input type="text"/>
         <div>
           <FaCompass />
@@ -14,6 +29,8 @@ class Navbar extends Component{
           <FaUser />
         </div>
       </div>
+
+      
     );
   }
 }
