8ccf5d020059b9f1731e8903f7b937162659718b
Skyoumans <skyyoumans@gmail.com>
2017-10-11 13:52:15 -0400

    changed post 1

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import Comment from './Comment';
 import { FaHeartO, FaCommentO } from 'react-icons/lib/fa'
+import styled from 'styled-components';
+
+
+const PostContainer = styled.div`
+  background-color: white;
+  border: 1px solid black;
+`
 
 class Post extends Component{
 
   render(){
     const { post } = this.props;
     return(
-      <div>
+      <PostContainer>
         <div>
           <img src={post.user.profile_pic} alt={post.user.username} />
           <p>{post.user.username}</p>
@@ -22,7 +29,7 @@ class Post extends Component{
         </div>
         <p>{post.likes.length} likes</p>
         {post.comments.map((comment,i) => <Comment key={i} comment={comment}/>)}
-      </div>
+      </PostContainer>
     )
   }
 }
