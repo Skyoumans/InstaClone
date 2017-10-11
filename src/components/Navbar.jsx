26e3c2d5b514113c157636fccf9dddc2ff902a8e
Skyoumans <skyyoumans@gmail.com>
2017-10-11 12:25:33 -0400

    changes 6

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,21 +4,25 @@ import styled from 'styled-components';
 
 
 const Nav = styled.div`
-  background-color: white;
-  display: flex;
-  justify-content: space-between;
-  padding: 26px 5vw;
-  width: 90vw;
-  height: 25px;
-  font-size: 22px;
-  font-family: 'Oleo Script', cursive;
-    p {
-
+background-color: white;
+display: flex;
+justify-content: space-between;
+align-items: center;
+padding: 26px 5vw;
+width: 90vw;
+height: 25px;
+border-bottom: 1px solid rgba(0,0,0,.0975);
+    h3 {
+    font-family: 'Oleo Script', cursive;
+    font-size: 26px;
     }
     search {
-
+    }
+    tags {
+      
     }
 `;
+
     
 class Navbar extends Component{
   render(){
@@ -29,11 +33,11 @@ class Navbar extends Component{
           <FaSearch/>
           <input type="text"/>
         </search>
-        <div>
+        <tags>
           <FaCompass />
           <FaHeartO />
           <FaUser />
-        </div>
+        </tags>
       </Nav>
 
       
